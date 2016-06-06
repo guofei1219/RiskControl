b6bfa0bf9047d9606fef74e4242600512c7fd932
guofei1219 <86608625@qq.com>
2016-06-06 13:51:50 +0800

    测试Streming连接kafka两种方式（receivor和direct）

---
+++ src/main/scala/kafka2streaming/DirectKafkaWordCount.scala
@@ -0,0 +1,83 @@
+package kafka2streaming
+
+/**
+  * Created by 郭飞 on 2016/6/6.
+  */
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+import kafka.serializer.StringDecoder
+
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.kafka._
+import org.apache.spark.SparkConf
+
+/**
+  * Consumes messages from one or more topics in Kafka and does wordcount.
+  * Usage: DirectKafkaWordCount <brokers> <topics>
+  *   <brokers> is a list of one or more Kafka brokers
+  *   <topics> is a list of one or more kafka topics to consume from
+  *
+  * Example:
+  *    $ bin/run-example streaming.DirectKafkaWordCount broker1-host:port,broker2-host:port \
+  *    topic1,topic2
+  */
+object DirectKafkaWordCount {
+  def main(args: Array[String]) {
+/*    if (args.length < 2) {
+      System.err.println(s"""
+                            |Usage: DirectKafkaWordCount <brokers> <topics>
+                            |  <brokers> is a list of one or more Kafka brokers
+                            |  <topics> is a list of one or more kafka topics to consume from
+                            |
+        """.stripMargin)
+      System.exit(1)
+    }*/
+
+    //StreamingExamples.setStreamingLogLevels()
+
+    //val Array(brokers, topics) = args
+    val brokers = "hc4:9092"
+    val topics = Set("user_events")
+
+
+    // Create context with 2 second batch interval
+    val sparkConf = new SparkConf().setAppName("DirectKafkaWordCount").setMaster("local[2]")
+    val ssc = new StreamingContext(sparkConf, Seconds(5))
+
+    //直连方式不支持还原点机制，需要自己记录偏移量
+    //ssc.checkpoint("checkpoint")
+
+    // Create direct kafka stream with brokers and topics
+    //val topicsSet = topics.split(",").toSet
+    val kafkaParams = Map[String, String]("metadata.broker.list" -> brokers)
+    val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](
+      ssc, kafkaParams, topics)
+
+    // Get the lines, split them into words, count the words and print
+    val lines = messages.map(_._2)
+    val words = lines.flatMap(_.split(" "))
+    val wordCounts = words.map(x => (x, 1L)).reduceByKey(_ + _)
+    wordCounts.print()
+
+    // Start the computation
+    ssc.start()
+    ssc.awaitTermination()
+  }
+}
+// scalastyle:on println
